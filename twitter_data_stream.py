d4a455037951eb602e87b412d25d56bdcf642c14
KNobles <nobles-k@hotmail.com>
2023-01-09 23:58:26 +0100

    removing unused vars

--- twitter_data_stream.py
+++ twitter_data_stream.py
@@ -6,10 +6,6 @@ from tweepy import StreamingClient, StreamRule
 load_dotenv()
 
 bearer_token = os.environ["BEARER_TOKEN"]
-# consumer_key = os.environ["API_KEY"]
-# consumer_secret = os.environ["API_KEY_SECRET"]
-# access_token = os.environ["ACCESS_TOKEN"]
-# access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
 class TweetPrinterV2(StreamingClient):
     
@@ -20,13 +16,14 @@ class TweetPrinterV2(StreamingClient):
 printer = TweetPrinterV2(bearer_token=bearer_token)
 
 # add new rules    
-rule = StreamRule(value="VFIO")
-printer.add_rules(rule)
+rule = StreamRule(value="giveaway")
 
-# # remove all rules
-# resp = printer.get_rules()
-# for data in resp.data:
-#     print(printer.delete_rules(ids=data.id))
-# # printer.filter()
+# remove all rules
+resp = printer.get_rules()
+for data in resp.data:
+    print(printer.delete_rules(ids=data.id))
+# printer.filter()
+
+printer.add_rules(rule)
 
 printer.filter(expansions="author_id",tweet_fields="created_at")
